e05932fcd673b7185be0554c7405952d992ac70c
ABOBA4853 <dsaga2011@gmail.com>
2023-05-19 21:43:05 +0300

    dz2.0-1

---
+++ dz0201/dz2.0-1.py
@@ -0,0 +1,9 @@
+def heloo_user (name):
+    print('Hello', name)
+heloo_user('Применко Андрій Борисович!')
+
+age=int(input('Вкажіть свік вік'))
+if (age<16):
+    print('Покажіть свідоцтво про народження')
+else:
+    print('Покажіть паспорт')
