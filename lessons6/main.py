d1e8065e3be63fee01eced33d3921f60e2177e63
ABOBA4853 <dsaga2011@gmail.com>
2023-05-27 14:34:26 +0300

    lessons5

---
+++ lessons6/main.py
@@ -0,0 +1,28 @@
+'''
+try:
+    your_code
+except type_of_Exception:
+    your_code
+except ......
+'''
+while(True):
+    try:
+        digit1 = int(input('Enter digit1: '))
+        digit2 = int(input('Enter digit2: '))
+#print(digit1/digit2)
+        yes = input('Do you want try again [Y/N]?: ')
+        if (yes.lower() != 'y'):
+            break
+
+    except ZeroDivisionError as zde:
+        print(f'Standart exception {zde}')
+        yes = input('You have to enter value not equal 0 for digit2\n'
+                    'Do you want try again [Y/N]?: ')
+        if (yes.lower() != 'y'):
+            break
+    except Exception as ex:
+        print(f'Standart exception {ex}')
+        yes = input('You have to enter right value as digit(int)\n'
+                    'Do you want try again [Y/N]?: ')
+        if(yes.lower() != 'y'):
+            break
