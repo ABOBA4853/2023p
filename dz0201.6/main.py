43360204006d362ea3c1f59484a3938025891734
ABOBA4853 <dsaga2011@gmail.com>
2023-05-27 20:19:32 +0300

    DZ2.0-6

---
+++ dz0201.6/main.py
@@ -0,0 +1,20 @@
+from colorama import  Fore, Back, Style
+def divider(a, b):
+
+   try:
+       return a / b
+   except ZeroDivisionError:
+       print(Fore.BLACK + Back.RED + Style.DIM + 'Ви не можете ділити на нуль!')
+       return 0
+   except TypeError:
+       print(Fore.RED + Back.BLACK + Style.DIM + 'Ви не можете ділити нечислові значення!')
+       return 0
+   except ValueError:
+       print(Fore.RED + Back.BLACK + Style.DIM + 'Ви не можете ділити нечислові значення!')
+       return 0
+data = {10: 2, 2: 5, '123': 4, 18: 0, 8: 4}
+result = []
+for key in data:
+   res = divider(key, data[key])
+   result.append(res)
+print(result)
