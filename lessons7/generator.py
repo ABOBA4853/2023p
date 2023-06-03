86be725d4b6267566465f6713688a6c5a522833f
ABOBA4853 <dsaga2011@gmail.com>
2023-06-03 13:52:53 +0300

    lesson7

--- lessons7/generator.py
+++ lessons7/generator.py
@@ -1,3 +1,11 @@
+'''
+class Generator:
+    def __init__(self, number = 0):
+        self.Number = number
+    def Pow(self, pow):
+        for i in range(1, pow+1):
+            yield self.Number ** i
+'''
 class Generator:
     def __init__(self, number = 0):
         self.Number = number
