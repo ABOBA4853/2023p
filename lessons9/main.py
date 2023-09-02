7e50163d149b9e10b60457bb257c15fcbd0cca50
ABOBA4853 <dsaga2011@gmail.com>
2023-09-02 13:18:15 +0300

    dz_2.0-09

--- lessons9/main.py
+++ lessons9/main.py
@@ -1,4 +1,12 @@
+#1
+'''
 import urllib.request
 opener = urllib.request.build_opener()
 response = opener.open("https://httpbin.org/get")
-print (response.read())
+print(response.read())
+'''
+
+#2
+import requests
+response = requests.get("https://httpbin.org/get")
+print(response.content)
