aea6243a8debe5726d9f4666b3fd6f29af62146e
ABOBA4853 <dsaga2011@gmail.com>
2023-09-08 23:08:46 +0300

    dz_09

---
+++ dz_09/main.py
@@ -0,0 +1,17 @@
+#1
+'''
+import urllib.request
+opener = urllib.request.build_opener()
+response = opener.open("https://httpbin.org/get")
+print(response.read())
+'''
+#2
+'''
+import requests
+response = requests.get("https://httpbin.org/get")
+print(response.content)
+'''
+from parserhtml import ParserHTML
+parser = ParserHTML("https://www.oschadbank.ua/")
+parser.ParseCoin("<span>", "</span>", "$")
+print(parser.Result)
