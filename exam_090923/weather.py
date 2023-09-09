c3bced6d0650def92a18b3fc57103711a48e584b
ABOBA4853 <dsaga2011@gmail.com>
2023-09-10 00:24:48 +0300

    dz_11-12

--- exam_090923/weather.py
+++ exam_090923/weather.py
@@ -1,25 +1,55 @@
+import sqlite3
 import requests
 from bs4 import BeautifulSoup
-import sqlite3
 
-response = requests.get("https://sinoptik.ua")
-if response.status_code == 200:
-    response = BeautifulSoup(response.text, "html.parser")
-    date = response.find_all("p", {"class": "date"})
-    month = response.find_all("p", {"class": "month"})
-    minTemp = response.find_all("div", {"class": "min"})
-    maxTemp = response.find_all("div", {"class": "max"})
+connection = sqlite3.connect("exam.sl3", 5)
+cur = connection.cursor()
+try:
+    cur.execute("CREATE TABLE first_table (name TEXT);")
+except sqlite3.OperationalError:
+    pass
 
-    db = sqlite3.connect("db.sl3", 5)
-    cur = db.cursor()
-
-    for i in range(7):
-
-        data = [date[i].text + " " + month[i].text, minTemp[i].text, maxTemp[i].text]
-        cur.execute("INSERT INTO temp (time, minTemp, maxTemp) VALUES (?, ?, ?)", data)
+cur.execute(f"SELECT rowid, name FROM first_table WHERE rowid={5};")
+res = cur.fetchall()
+print(res)
 
 
-    db.commit()
-    db.close()
-else:
-    print("Cannot connect to the server! Error " + str(response.status_code))
+cur.execute(f"SELECT rowid, name FROM first_table WHERE rowid=1;")
+res = cur.fetchall()
+if res == []:
+    cur.execute("INSERT INTO first_table (name) VALUES (' ');")
+cur.execute(f"SELECT rowid, name FROM first_table WHERE rowid=2;")
+res = cur.fetchall()
+if res == []:
+    cur.execute("INSERT INTO first_table (name) VALUES (' ');")
+
+
+
+
+resp = requests.get("https://ua.sinoptik.ua/погода-святопетрівське")
+if resp.status_code == 200:
+    soup = BeautifulSoup(resp.text, features = "html.parser")
+    soup_list = soup.find_all('p')
+    soup_list = str(soup_list[23]).split('<p class="today-time">Погода сьогодні о ')
+    soup_list = str(soup_list[1]).split('</p>')
+    time = str(soup_list[0])
+if resp.status_code == 200:
+    soup = BeautifulSoup(resp.text, features = "html.parser")
+    soup_list = soup.find_all('p')
+    soup_list = str(soup_list[24]).split('<p class="today-temp">')
+    soup_list = str(soup_list[1]).split('</p>')
+    temperature = str(soup_list[0])
+if resp.status_code == 200:
+    soup = BeautifulSoup(resp.text, features = "html.parser")
+    soup_list = soup.find_all('p')
+    soup_list = str(soup_list[2]).split('p class="day-link" data-link="//ua.sinoptik.ua/погода-святопетрівське/')
+    soup_list = str(soup_list[1]).split('">Субота</p>')
+    date = str(soup_list[0])
+
+
+cur.execute(f"UPDATE first_table SET name='{date}: {time}' WHERE rowid=1;")
+cur.execute(f"UPDATE first_table SET name='{temperature}' WHERE rowid=2;")
+
+connection.commit()
+
+connection.close()
